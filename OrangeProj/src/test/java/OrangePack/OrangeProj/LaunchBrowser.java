1924cd01652caae30a1b43d6e72772628f707a31
AmolNater <anatersh@gmail.com>
2020-07-27 10:22:19 +0530

    updated

--- OrangeProj/src/test/java/OrangePack/OrangeProj/LaunchBrowser.java
+++ OrangeProj/src/test/java/OrangePack/OrangeProj/LaunchBrowser.java
@@ -2,16 +2,26 @@ package OrangePack.OrangeProj;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class LaunchBrowser 
 {
+	public WebDriver driver;
+	@Parameters("browser")
     @Test
-    public void launchBrowser() {
+    public void launchBrowser(String browserName) {
     	System.out.println("Test Code");
-		WebDriverManager.chromedriver().setup();
-		WebDriver driver=new ChromeDriver();
+    	if(browserName.equalsIgnoreCase("chrome")) {
+    		WebDriverManager.chromedriver().setup();
+    		driver=new ChromeDriver();	
+    	}else if(browserName.equalsIgnoreCase("firefox")) {
+    		WebDriverManager.firefoxdriver().setup();
+    		driver=new FirefoxDriver();
+    	}
+		driver.get("https://opensource-demo.orangehrmlive.com/");
     }
 }
